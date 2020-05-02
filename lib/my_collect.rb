b6ad702bb3f5b9ef92e029096d8b047294a0fd71
Bobby Neal <rsneal01@gmail.com>
2020-05-02 17:34:05 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,7 @@ def my_collect(array)
   i = 0
   capitalized_languages = []
   while i < array.length
-    capitalized_languages << 
-    yield(array[i])
+    capitalized_languages << yield(array[i])
     i += 1
   end
   # array
