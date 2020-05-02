e3c494d7f4b2d175c5703a77e191ebbf741fd19b
Bobby Neal <rsneal01@gmail.com>
2020-05-02 17:29:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(array)
   i = 0
   while i < array.length
-  languages do |language|
-    language.upcase
-  end  
+  # languages do |language|
+  #   language.upcase
+  # end  
     yield(array[i])
     i += 1
   end
